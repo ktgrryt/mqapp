aa5f06ad2f0c460efe3dbe9deff32ec210392947
ktgr0425 <ktgrryt@gmail.com>
2025-04-09 19:19:08 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+
+FROM icr.io/appcafe/open-liberty:kernel-slim-java17-openj9-ubi
+
+COPY --chown=1001:0 /src/main/liberty/config /config
+
+COPY --chown=1001:0 ibm/wmq.jakarta.jmsra.rar /config
+
+RUN features.sh
+
+COPY --chown=1001:0 target/*.war /config/apps
+
+RUN configure.sh
---
+++ src/main/java/com/demo/rest/MessageApi.java
@@ -0,0 +1,30 @@
+package com.demo.rest;
+
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import com.demo.messaging.MQProducer;
+
+@Stateless
+@Path("/")
+public class MessageApi {
+
+    @Inject
+    MQProducer producer;
+
+    @GET
+    @Path("/ping")
+    public String ping(){
+        return "OK";
+    }
+
+    @POST
+    @Path("/send")
+    public String send(@FormParam("msg") String message) throws Exception {
+        // 1件メッセージ送信
+        return producer.sendMessage(message);
+    }
+}
